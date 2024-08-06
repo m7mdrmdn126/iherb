152418c0c2a03c7006d06ae70309a5a9507bc8d5
Mazen Balat <mazenbalatt@gmail.com>
2024-08-06 05:20:38 +0300

    update db.js

--- db/db.js
+++ db/db.js
@@ -1,16 +1,10 @@
-sqlite3 = require('sqlite3')
+const sqlite3 = require('sqlite3');
 
-let db = new sqlite3.Database('D:/rmdn/iherb/site/db/iherb.db', (err) => {
+let db = new sqlite3.Database('C:\Users\mazen\OneDrive\Desktop\IHerb\IHerb\iherb\db\iherb.db', (err) => {
     if (err) {
         console.error(err.message);
     }
     console.log('Connected to the SQLite database.');
 });
 
-
-
-
-
-module.exports = db
-
-
+module.exports = db;
