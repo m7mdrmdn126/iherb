700f44a1155531c71d47272b28186d03e3d5f4b8
Mazen Balat <mazenbalatt@gmail.com>
2024-08-06 05:22:20 +0300

    add para for express

--- server.js
+++ server.js
@@ -2,3 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ordersController = require('./controllers/orders');
 const productsController = require('./controllers/products');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
