54f14700ced2d262613b2325424f7f4455d93ee8
Mazen Balat <mazenbalatt@gmail.com>
2024-08-06 05:21:38 +0300

    add packages in server.js

---
+++ server.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ordersController = require('./controllers/orders');
+const productsController = require('./controllers/products');
